05341796f44dfda106edc088a4950fd995602113
callistusystan <callistusystan@gmail.com>
2018-07-28 16:24:12 +1000

    Setup homepage

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -4,13 +4,13 @@ class Logo extends Component {
     render() {
         return (
             <div style={{ margin: 'auto', display: 'flex' }}>
-                <h2 style={{
+                <span style={{
                     fontSize: 24,
                     fontWeight: 700,
                     backgroundColor: '#FF5B5B',
                     padding: '2px 4px',
                     color: '#FFF', ...this.props.mediStyle
-                }}>MEDI</h2>
+                }}>MEDI</span>
                 <p style={{
                     fontSize: 24,
                     padding: 2,
